73efe14bf954cff8ed8fa131ffe9869a06b19603
IrM <49803320+IrinaMudrov@users.noreply.github.com>
2019-04-30 22:37:14 -0700

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,19 @@
+# Python program to draw sun
+# using Turtle Programming 
+import turtle  
+sun = turtle.Turtle() 
+wn = turtle.Screen()
+wn.bgcolor("light blue")
+
+  
+for i in range(26): 
+    sun.color("yellow")
+    sun.forward(250) 
+    sun.right(244)
+    
+turtle.done() 
+       
+      
+
+
+
